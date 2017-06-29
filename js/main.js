ee5c9353dded1040995c9339b6481ad85ca1ac1e
punya528 <punya528@hotmail.com>
2017-06-29 16:00:38 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,32 +7,32 @@ TweenLite.to(photo, 1.5, {width:100});*/
 
 function setCenterBtn(value){
   var Btn = document.getElementById('centerBtn');
-  Btn.src='images/centerBtn'+value+'.png'
+  Btn.src='images/index/centerBtn'+value+'.png'
 }
 
 function set3dWebBtn(value){
   var Btn = document.getElementById('3dWebBtn');
-  Btn.src='images/3dWeb'+value+'.png'
+  Btn.src='images/index/3dWeb'+value+'.png'
 }
 
 function setPrintingBtn(value){
   var Btn = document.getElementById('printingBtn');
-  Btn.src='images/printing'+value+'.png'
+  Btn.src='images/index/printing'+value+'.png'
 }
 
 function setVtrBtn(value){
   var Btn = document.getElementById('vtrBtn');
-  Btn.src='images/vtr'+value+'.png'
+  Btn.src='images/index/vtr'+value+'.png'
 }
 
 function setEventBtn(value){
   var Btn = document.getElementById('EventBtn');
-  Btn.src='images/Event'+value+'.png'
+  Btn.src='images/index/Event'+value+'.png'
 }
 
 function setBtn(id,value){
   var Btn = document.getElementById(id);
-  Btn.src='images/'+value;
+  Btn.src='images/index/'+value;
 }
 
 function debug(value) {
@@ -43,6 +43,18 @@ function setOpacity(value) {
   var svgBtn = document.getElementsByClassName("svgBtn");
   for (i = 0; i < svgBtn.length; i++) {
     svgBtn[i].style.opacity = value;
+    //console.log(svgBtn[i].points)
   }
 }
 window.onload = setOpacity(0);
+
+function setCenterMain(){
+  var main = document.getElementById("main");
+  var cMain = document.getElementsByClassName("main");
+  console.log(cMain[0].style)
+  var contrainer = document.getElementsByClassName("contrainer");
+  main.style.top = ((window.innerHeight / 2)-345 ) + "px";
+  main.style.left = (window.innerWidth / 2)-420+"px";
+
+}
+window.onload = setCenterMain();
